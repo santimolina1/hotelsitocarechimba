227e437c977c05383e19d783b0b133fdf4a669c8
JuanJoseTovar <93229908+JuanJoseTovar@users.noreply.github.com>
2023-05-07 12:44:49 -0500

    check in y correccion reserva

--- HotelAndes/src/interfaz/agregar_huesped.java
+++ HotelAndes/src/interfaz/agregar_huesped.java
@@ -1,11 +1,11 @@
 package interfaz;
 
-public class realizar_reserva extends javax.swing.JFrame {
+public class agregar_huesped extends javax.swing.JFrame {
 
     /**
-     * Creates new form Realizar_Reserva
+     * Creates new form agregar_huesped
      */
-    public realizar_reserva() {
+    public agregar_huesped() {
         initComponents();
     }
 
@@ -16,21 +16,16 @@ public class realizar_reserva extends javax.swing.JFrame {
      */
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
-    // </editor-fold>   
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jTextField1 = new javax.swing.JTextField();
         jTextField2 = new javax.swing.JTextField();
-        jTextField3 = new javax.swing.JTextField();
-        jTextField4 = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(null);
@@ -40,9 +35,9 @@ public class realizar_reserva extends javax.swing.JFrame {
         jPanel1.setLayout(null);
 
         jLabel1.setFont(new java.awt.Font("Helvetica Neue", 0, 16)); // NOI18N
-        jLabel1.setText("Datos de reserva:");
+        jLabel1.setText("Agregar huésped:");
         jPanel1.add(jLabel1);
-        jLabel1.setBounds(170, 10, 130, 20);
+        jLabel1.setBounds(170, 30, 130, 20);
 
         jTextField1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -50,18 +45,12 @@ public class realizar_reserva extends javax.swing.JFrame {
             }
         });
         jPanel1.add(jTextField1);
-        jTextField1.setBounds(170, 60, 190, 30);
+        jTextField1.setBounds(170, 100, 190, 30);
         jPanel1.add(jTextField2);
-        jTextField2.setBounds(170, 100, 190, 30);
-        jPanel1.add(jTextField3);
-        jTextField3.setBounds(170, 140, 190, 30);
-
-        jTextField4.setToolTipText("");
-        jPanel1.add(jTextField4);
-        jTextField4.setBounds(170, 180, 190, 30);
+        jTextField2.setBounds(170, 160, 190, 30);
 
         jButton1.setBackground(new java.awt.Color(255, 249, 132));
-        jButton1.setText("Ingresar");
+        jButton1.setText("Agregar");
         jPanel1.add(jButton1);
         jButton1.setBounds(170, 240, 110, 30);
 
@@ -72,19 +61,11 @@ public class realizar_reserva extends javax.swing.JFrame {
 
         jLabel2.setText("Nombre");
         jPanel1.add(jLabel2);
-        jLabel2.setBounds(50, 70, 46, 17);
+        jLabel2.setBounds(60, 110, 46, 17);
 
-        jLabel3.setText("# Acompañantes");
+        jLabel3.setText("Cédula");
         jPanel1.add(jLabel3);
-        jLabel3.setBounds(50, 110, 98, 17);
-
-        jLabel4.setText("Fecha Inicio");
-        jPanel1.add(jLabel4);
-        jLabel4.setBounds(50, 150, 80, 17);
-
-        jLabel5.setText("Fecha salida");
-        jPanel1.add(jLabel5);
-        jLabel5.setBounds(50, 190, 74, 17);
+        jLabel3.setBounds(60, 170, 41, 17);
 
         getContentPane().add(jPanel1);
         jPanel1.setBounds(0, 0, 460, 330);
@@ -94,7 +75,7 @@ public class realizar_reserva extends javax.swing.JFrame {
 
     private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {                                            
         // TODO add your handling code here:
-    } 
+    }                                           
 
     /**
      * @param args the command line arguments
@@ -113,7 +94,7 @@ public class realizar_reserva extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(realizar_reserva.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(agregar_huesped.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
         
@@ -121,7 +102,7 @@ public class realizar_reserva extends javax.swing.JFrame {
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(() -> {
-            new realizar_reserva().setVisible(true);
+            new agregar_huesped().setVisible(true);
         });
     }
 
@@ -131,12 +112,8 @@ public class realizar_reserva extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JTextField jTextField1;
     private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
-    private javax.swing.JTextField jTextField4;
     // End of variables declaration                   
 }
--- HotelAndes/src/interfaz/check_in.java
+++ HotelAndes/src/interfaz/check_in.java
@@ -1,11 +1,11 @@
 package interfaz;
 
-public class realizar_reserva extends javax.swing.JFrame {
+public class check_in extends javax.swing.JFrame {
 
     /**
-     * Creates new form Realizar_Reserva
+     * Creates new form Check_in
      */
-    public realizar_reserva() {
+    public check_in() {
         initComponents();
     }
 
@@ -16,21 +16,13 @@ public class realizar_reserva extends javax.swing.JFrame {
      */
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
-    // </editor-fold>   
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
-        jTextField2 = new javax.swing.JTextField();
-        jTextField3 = new javax.swing.JTextField();
-        jTextField4 = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(null);
@@ -39,62 +31,42 @@ public class realizar_reserva extends javax.swing.JFrame {
         jPanel1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
         jPanel1.setLayout(null);
 
-        jLabel1.setFont(new java.awt.Font("Helvetica Neue", 0, 16)); // NOI18N
-        jLabel1.setText("Datos de reserva:");
+        jLabel1.setFont(new java.awt.Font("Helvetica Neue", 0, 18)); // NOI18N
+        jLabel1.setText("El archivo a cargar,");
         jPanel1.add(jLabel1);
-        jLabel1.setBounds(170, 10, 130, 20);
+        jLabel1.setBounds(160, 110, 156, 23);
 
-        jTextField1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField1ActionPerformed(evt);
-            }
-        });
-        jPanel1.add(jTextField1);
-        jTextField1.setBounds(170, 60, 190, 30);
-        jPanel1.add(jTextField2);
-        jTextField2.setBounds(170, 100, 190, 30);
-        jPanel1.add(jTextField3);
-        jTextField3.setBounds(170, 140, 190, 30);
-
-        jTextField4.setToolTipText("");
-        jPanel1.add(jTextField4);
-        jTextField4.setBounds(170, 180, 190, 30);
+        jLabel2.setFont(new java.awt.Font("Helvetica Neue", 0, 18)); // NOI18N
+        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel2.setText("ha sido cargado con éxito.");
+        jPanel1.add(jLabel2);
+        jLabel2.setBounds(130, 140, 220, 23);
 
         jButton1.setBackground(new java.awt.Color(255, 249, 132));
-        jButton1.setText("Ingresar");
+        jButton1.setText("Volver");
         jPanel1.add(jButton1);
-        jButton1.setBounds(170, 240, 110, 30);
+        jButton1.setBounds(280, 200, 110, 30);
 
         jButton2.setBackground(new java.awt.Color(255, 249, 132));
-        jButton2.setText("Volver");
+        jButton2.setText("Agregar huésped");
+        jButton2.setToolTipText("");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
         jPanel1.add(jButton2);
-        jButton2.setBounds(330, 290, 110, 30);
-
-        jLabel2.setText("Nombre");
-        jPanel1.add(jLabel2);
-        jLabel2.setBounds(50, 70, 46, 17);
-
-        jLabel3.setText("# Acompañantes");
-        jPanel1.add(jLabel3);
-        jLabel3.setBounds(50, 110, 98, 17);
-
-        jLabel4.setText("Fecha Inicio");
-        jPanel1.add(jLabel4);
-        jLabel4.setBounds(50, 150, 80, 17);
-
-        jLabel5.setText("Fecha salida");
-        jPanel1.add(jLabel5);
-        jLabel5.setBounds(50, 190, 74, 17);
+        jButton2.setBounds(90, 200, 140, 30);
 
         getContentPane().add(jPanel1);
-        jPanel1.setBounds(0, 0, 460, 330);
+        jPanel1.setBounds(0, 0, 450, 320);
 
         pack();
     }// </editor-fold>                        
 
-    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {                                            
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {                                         
         // TODO add your handling code here:
-    } 
+    }                                        
 
     /**
      * @param args the command line arguments
@@ -113,7 +85,7 @@ public class realizar_reserva extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(realizar_reserva.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(check_in.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
         
@@ -121,7 +93,7 @@ public class realizar_reserva extends javax.swing.JFrame {
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(() -> {
-            new realizar_reserva().setVisible(true);
+            new check_in().setVisible(true);
         });
     }
 
@@ -130,13 +102,6 @@ public class realizar_reserva extends javax.swing.JFrame {
     private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
-    private javax.swing.JTextField jTextField4;
     // End of variables declaration                   
 }
